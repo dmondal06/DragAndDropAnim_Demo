f599f029df50e7c45853ab36ddac1f6f60ef0870
dmondal06 <deepa.mondal321@gmail.com>
2024-11-27 23:01:44 -0500

    Completed Todo 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.15"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -48,7 +48,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.compose.animation:animation:1.7.5")
+    implementation(libs.androidx.animation)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
